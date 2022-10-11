dc03da0d5d883220f2c8d4395857d6e2ee59eedb
johnmaster <juanpuhl404@gmail.com>
2022-10-10 21:28:58 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tp6_4_suma_2_enteros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, suma;
+
+            Console.WriteLine("Ingrese el numero 1: ");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero 2: ");
+            num2=Convert.ToInt32(Console.ReadLine());
+            suma= num1 + num2;
+            Console.WriteLine("La suma de: " + num1 + " y " +  num2  + " es: " +suma);
+            Console.ReadKey(); 
+        }
+    }
+}
